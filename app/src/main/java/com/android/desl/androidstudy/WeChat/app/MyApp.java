a3d30ec9d93f127a536144ea6f97b06ae15f3cc4
王涛 <desl0451@hotmail.com>
2019-01-16 17:20:57 +0800

    2019-01-16 17:20 更新

--- app/src/main/java/com/android/desl/androidstudy/WeChat/app/MyApp.java
+++ app/src/main/java/com/android/desl/androidstudy/WeChat/app/MyApp.java
@@ -2,6 +2,8 @@ package com.android.desl.androidstudy.WeChat.app;
 
 import com.android.desl.androidstudy.WeChat.app.base.BaseApp;
 
+import io.rong.imlib.RongIMClient;
+
 /**
  * ━━━━━━神兽出没━━━━━━
  * 　　  ┏┓     ┏┓
@@ -25,5 +27,12 @@ import com.android.desl.androidstudy.WeChat.app.base.BaseApp;
  * BaseApp的拓展，用于设置其他第三方的初始化
  */
 
-public class MyApp extends BaseApp {
+public class MyApp extends BaseApp implements RongIMClient.OnReceiveDestructionMessageListener {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //初始化融云
+//        initRongCloud();
+    }
 }
